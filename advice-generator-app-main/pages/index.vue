6fe93f77773272db38fd7e2c2bb3b5e61d44856b
Saeed1112 <nullee.sa@gmail.com>
2023-03-13 11:36:06 +0330

    Update index.vue

--- advice-generator-app-main/pages/index.vue
+++ advice-generator-app-main/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="min-h-screen bg-dark-blue font-manrope flex flex-col justify-center items-center px-3">
 
-    <div class="shadow-2xl bg-dark-grayish-blue mt-auto rounded-xl px:8 sm:px-12 py-10 w-full sm:w-[32rem] relative flex flex-col gap-5">
+    <div class="shadow-2xl bg-dark-grayish-blue mt-auto rounded-xl px-8 sm:px-12 py-10 w-full sm:w-[32rem] relative flex flex-col gap-5">
       <h2 class="text-neon-green uppercase tracking-[4px] text-center font-semibold text-sm">Advice #{{ data?.id }}</h2>
       <p class="text-light-cyan font-bold text-[1.75rem]">“{{ data?.advice }}”</p>
       <img src="/images/pattern-divider-desktop.svg" class="mb-8 hidden sm:inline" alt="image">
