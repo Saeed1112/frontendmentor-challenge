bc535e854dd380c52e17edeef967579b7a5f999e
Saeed1112 <nullee.sa@gmail.com>
2023-03-27 13:42:46 +0430

    change classes

--- 3-column-preview-card-component-main/pages/index.vue
+++ 3-column-preview-card-component-main/pages/index.vue
@@ -3,7 +3,7 @@
     <div
         class="w-full sm:w-[20rem] md:w-[30rem] lg:w-[57.5rem] bg-gray-50 flex flex-col lg:flex-row overflow-hidden rounded-xl my-auto">
       <div v-for="(item,key) in items" :key="key"
-           :class="`bg-${item.color}`"
+           :class="`${item.color}`"
            class="flex-1 p-12 text-white flex flex-col gap-10"
       >
         <div>
@@ -13,8 +13,7 @@
         <p class="text-15 text-white text-opacity-75 font-lexend-deca">{{ item.content }}</p>
         <div class="mt-0 md:mt-12">
           <a href="#"
-             :class="`hover:text-${item.color}`"
-             class="capitalize hover:bg-white font-lexend-deca font-light text-white text-opacity-75 px-6 py-3 border-2 border-white rounded-full">
+             class="capitalize learn-more hover:bg-white font-lexend-deca font-light text-white text-opacity-75 px-6 py-3 border-2 border-white rounded-full">
             Learn more
           </a>
         </div>
@@ -28,19 +27,19 @@ const items = [
   {
     title: 'Sedans',
     content: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-    color: 'bright-orange',
+    color: 'sedans',
     image: '/images/icon-sedans.svg'
   },
   {
     title: 'SUVs',
     content: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-    color: 'dark-cyan',
+    color: 'suvs',
     image: '/images/icon-suvs.svg'
   },
   {
     title: 'Luxury',
     content: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-    color: 'very-dark-cyan',
+    color: 'luxury',
     image: '/images/icon-luxury.svg'
   }
 ]
@@ -48,7 +47,30 @@ const items = [
 
 <style scoped>
 .init {
-  @apply bg-bright-orange bg-dark-cyan bg-very-dark-cyan;
-  @apply hover:text-very-dark-cyan hover:text-bright-orange hover:text-dark-cyan ;
+  @apply hover:text-very-dark-cyan hover:text-bright-orange hover:text-dark-cyan;
+}
+
+.suvs {
+  @apply bg-dark-cyan;
+}
+
+.suvs .learn-more {
+  @apply hover:text-very-dark-cyan;
+}
+
+.luxury {
+  @apply bg-very-dark-cyan;
+}
+
+.luxury .learn-more {
+  @apply hover:text-very-dark-cyan;
+}
+
+.sedans {
+  @apply bg-bright-orange;
+}
+
+.sedans .learn-more {
+  @apply hover:text-bright-orange;
 }
 </style>
